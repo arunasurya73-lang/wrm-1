1c0d587438c839d8d366f5b1198e67c822c64df7
Kaviya Selvan <kaviya@example.com>
2026-09-21 13:03:44 +0530

    fix(vercel): set framework null and remove unused React src folder to prevent framework detection failure

--- src/components/Navigator/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Navigator } from './Navigator.jsx';
-export { default as NavigatorButton } from './NavigatorButton.jsx';
-export { default as NavigatorPopup } from './NavigatorPopup.jsx';
